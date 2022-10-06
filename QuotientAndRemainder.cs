1f8ecaaf32e008de2abf72444e7786fad6ef993d
jayukasode23 <jayukasode2398@gmail.com>
2022-10-06 20:50:19 +0530

    Added Quatient And Remainder

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class QuotientAndRemainder
+    {
+        public static void CheckQuotientAndRemainder()
+        {
+            int divident = 60, divisor = 9;
+
+            int quotient = divident / divisor;
+            int remainder = divident % divisor;
+
+            Console.WriteLine("Quatient :" + quotient);
+            Console.WriteLine("Remainder :" + remainder);
+        }
+    }
+}
