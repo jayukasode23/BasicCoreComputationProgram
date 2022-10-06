551447f480b84be79f5d7b004d421a7e70a75ef2
jayukasode23 <jayukasode2398@gmail.com>
2022-10-06 20:53:24 +0530

    Added Even Or Odd Number

---
+++ EvenOrOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class EvenOrOdd
+    {
+        public static void CheckEvenOdd()
+        {
+            int num = 15;
+            int c = num % 2;
+
+            if (c == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+            }
+        }
+    }
+}
+
