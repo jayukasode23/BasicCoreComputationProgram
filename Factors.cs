3665b949b6d6b2d11b420af81f6a7fc8ba0393cf
jayukasode23 <jayukasode2398@gmail.com>
2022-10-06 20:48:43 +0530

    Added Prime Factors

---
+++ Factors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Factors
+    {
+        public static void CheckPrimeFactors()
+        {
+
+            Console.WriteLine("Enter the value of number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int factor;
+            for (factor = 1; factor <= number; factor++)
+            {
+                if (number % factor == 0)
+                {
+                    Console.WriteLine(factor + " , ");
+                }
+            }
+        }
+    }
+}
