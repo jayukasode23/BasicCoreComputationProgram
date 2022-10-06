65cdeb29eccf78885be8efd4a35f56be0f10b599
jayukasode23 <jayukasode2398@gmail.com>
2022-10-06 20:46:59 +0530

    Added Harmonic Number

---
+++ HarmonicNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class HarmonicNumber
+    {
+        public static void CheckHarmonicNumber()
+        {
+            Console.WriteLine("Enter The Value=");
+            int Value = (Convert.ToInt32(Console.ReadLine()));
+
+            double sum = 0;
+            Console.WriteLine("1");
+
+            for (double i = 1; i <= Value; i++)
+            {
+                Console.WriteLine(" +1/" + i);
+                sum += 1 / (float)i;
+
+            }
+            Console.WriteLine("+sum");
+
+        }
+    }
+}
