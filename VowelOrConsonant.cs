249bc821c7382916c6047525ab82581df80e1019
jayukasode23 <jayukasode2398@gmail.com>
2022-10-06 20:54:56 +0530

    Added Vowel Or Consonant

---
+++ VowelOrConsonant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class VowelOrConsonant
+    {
+        public static void Vowel()
+        {
+            Console.WriteLine("Enter any Character");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + "Is Vowel");
+
+            }
+
+
+        }
+    }
+}
