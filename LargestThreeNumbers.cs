fb1db401aedf5af15de9bf2076b4e22e40eb4146
jayukasode23 <jayukasode2398@gmail.com>
2022-10-06 20:56:52 +0530

    Added Largest Among Three Numbers

---
+++ LargestThreeNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class LargestThreeNumbers
+    {
+        public static void CheckNumbers()
+        {
+            int num1, num2, num3;
+
+            Console.WriteLine("Find the Largest of Three Numbers:  \n");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Input 1st number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input 2ndnumber: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input 3rd number: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("The 1st Number Greatest among of three. n\n");
+                }
+                else
+                {
+                    Console.WriteLine("The 3rd number Greatest among of three. n\n");
+                }
+            }
+            else if (num2 > num3)
+                Console.WriteLine("The 2nd number Gretest among of three. n\n");
+            else
+                Console.WriteLine("The 3rd number Gretest among three. n\n");
+        }
+    }
+}
