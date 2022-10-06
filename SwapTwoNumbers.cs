bff994e85439f44fc34ec21b7d765fd31d9b5caf
jayukasode23 <jayukasode2398@gmail.com>
2022-10-06 20:51:54 +0530

    Added Swap Two Numbers

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class SwapTwoNumbers
+    {
+        public static void SwapNumbers()
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Befor swap a = " + a + " b = " + b);
+            a = a * b;
+            b = a / b;
+            b = a / b;
+
+            Console.WriteLine("Before swap a = " + a + " b = " + b);
+        }
+    }
+}
