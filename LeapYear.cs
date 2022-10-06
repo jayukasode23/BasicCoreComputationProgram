e1cc908bb1dbe53ab12a432324034820acd4acfb
jayukasode23 <jayukasode2398@gmail.com>
2022-10-06 20:44:01 +0530

    Added Leap Year

---
+++ LeapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class LeapYear
+    {
+        public static void CheckLeapYear()
+        {
+            int year;
+            Console.WriteLine("Enter any year");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)))
+            {
+                Console.WriteLine("Leap Year " + year);
+            }
+            else
+                Console.WriteLine("Not Leap Year " + year);
+
+        }
+
+    }
+}
